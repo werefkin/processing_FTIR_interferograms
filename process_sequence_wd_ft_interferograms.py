749809519ccce5ab064d41f2281aab9da53aec36
Werefkin <zorin.ial@gmail.com>
2024-02-09 12:09:41 +0100

    Phase spectrum added

--- process_sequence_wd_ft_interferograms.py
+++ process_sequence_wd_ft_interferograms.py
@@ -11,42 +11,59 @@ import scipy.signal
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 import numpy as np
-from scipy.fft import fft  # , fftfreq, fftshift
+from scipy.fft import fft, fftshift  # , fftfreq, fftshift
 import os
 from datetime import datetime
-from scipy import signal
+
 
 # Init params
 lambdanehe = 632.8941914224686 * 1e-9  # 1/15800.429417*1e7 REF LASER
-crop_size = 50000  # Adjust this value as needed
+crop_size = 30000  # Adjust this value as needed
 pad_factor = 4
+sigma = 0  # Adjust the standard deviation (sigma) as needed
+
+
+# Plot each FT result
+plotft = False
+
 
 # Define the folder paths for reference signal (HeNe laser C3) and IR interferograms (C4)
-folder_c3 = "./c3/"
-folder_c4 = "./c4/"
+ref_laser_folder = "./c3/"
+interferograms_folder = "./c1/"
+
+
+# Get frequency axis
+N = 2 * crop_size * pad_factor
+upper_frequ = 1 / (lambdanehe / 2 * 1e2)  # *2 #because we sample
+delta_nu = upper_frequ / N  # 1e-2 to convertit to cm
+# Length of the FFT result
+deltaf = 1 / (N * delta_nu)
+freqs = fftshift(np.fft.fftfreq(N, d=deltaf))  # shift to make a plot with no artifacts (in the natural order)
+
 
 # Initialize lists to store dataframes for each folder
 dataframes_c3 = []
 dataframes_c4 = []
 
+
 # Get the list of file names in each folder
 file_list_c3 = sorted(
-    [f for f in os.listdir(folder_c3) if f.endswith(".csv")])[1:]
+    [f for f in os.listdir(ref_laser_folder) if f.endswith(".csv")])[2:]
 file_list_c4 = sorted(
-    [f for f in os.listdir(folder_c4) if f.endswith(".csv")])[1:]
+    [f for f in os.listdir(interferograms_folder) if f.endswith(".csv")])[2:]
 
 
 # Load data into dataframes for C3
 for file_name in file_list_c3:
-    print('C3 load file: ' + file_name)
-    file_path = os.path.join(folder_c3, file_name)
+    print('Ref load file: ' + file_name)
+    file_path = os.path.join(ref_laser_folder, file_name)
     df = pd.read_csv(file_path, skiprows=2, usecols=[0])
     dataframes_c3.append(df)
 
 # Load data into dataframes for C4
 for file_name in file_list_c4:
-    print('C4 load file: ' + file_name)
-    file_path = os.path.join(folder_c4, file_name)
+    print('Interferograms load file: ' + file_name)
+    file_path = os.path.join(interferograms_folder, file_name)
     df = pd.read_csv(file_path, skiprows=2, usecols=[0])
     dataframes_c4.append(df)
 
@@ -72,21 +89,16 @@ for i, df in enumerate(dataframes_c4):
 # Convert to numpy arrays
 array1 = array_3d_c3
 array2 = array_3d_c4
-sigma = 0  # Adjust the standard deviation (sigma) as needed
 # Define the number of elements to keep on each side
 
+# Initialize empthy arrays for the data
 spectra = np.zeros([crop_size * 2 * pad_factor, np.shape(array1)[1]])
+kernels = np.zeros([crop_size * 2 * pad_factor, np.shape(array1)[1]])
+angle_spectra = np.zeros([crop_size * 2 * pad_factor, np.shape(array1)[1]])
 
 
-N = 2 * crop_size
-# the more simple way:
-upper_frequ = 1 / (lambdanehe * 1e2)
-delta_nu = upper_frequ / N  # 1e-2 to convertit to cm
-# *2 #because the fft gives the spectrum twice and i dont cut it
-wn_axis = 2 / pad_factor * np.arange(N * pad_factor) * delta_nu
-bh_window = signal.windows.blackmanharris(N)  # blackman_harris window
-
-
+bh_window = np.blackman(2 * crop_size * pad_factor)  # blackman_harris window
+# Resampling, windowing, padding
 for i in range(0, np.shape(array1)[1]):
     # Apply Gaussian filter
     array_ref = gaussian_filter(array1[:, i], sigma)  # reference
@@ -106,27 +118,38 @@ for i in range(0, np.shape(array1)[1]):
     interferogram = array2[combined_indices, i]
 
     # Find the index of the maximum value in array2
-    max_index = np.argmin(interferogram)
+    max_index = np.argmax(interferogram)
 
-    centered_interf = interferogram[max_index -
-                                    crop_size:max_index + crop_size] * (bh_window)
-
+    centered_interf = interferogram[max_index - crop_size:max_index + crop_size]
     centered_interf_padded = np.pad(centered_interf,
-                                    (crop_size * (pad_factor - 1),
-                                     crop_size * (pad_factor - 1)),
+                                    (crop_size * (pad_factor - 1), crop_size * (pad_factor - 1)),
                                     mode='constant')
-    kernel = (centered_interf_padded -
-              np.mean(centered_interf_padded))
 
-    # abs of fourier transform with zerofilling
-    spectra[:, i] = np.abs(fft(kernel, norm="ortho"))
-    plt.plot(wn_axis, np.abs(spectra[:, i]))
-    plt.xlim([2126, 3400])
-    plt.ylim([0, None])
-    plt.xlabel('Wavenumber (cm^-1)')
-    plt.ylabel('PSD (a.u)')
-    plt.title('Reconstructed spectrum')
-    plt.savefig('reconstructed_spectrum.png', dpi=300, bbox_inches='tight')
+    kernel = (centered_interf_padded - np.mean(centered_interf_padded)) * (bh_window)
+    kernels[:, i] = kernel
+
+
+#  FFT
+for i in range(0, np.shape(array1)[1]):
+    print('Interferograms processed: ', i)
+    spec = fftshift(fft(fftshift(kernels[:, i]), norm="ortho"))  # fourier transform with zerofillingng
+    spectra[:, i] = np.abs(spec)
+    angle_spectra[:, i] = np.angle(spec)
+
+    if plotft is True:
+        plt.plot(freqs, np.abs(spectra[:, i]))
+        plt.xlim([2126, 3400])
+        plt.ylim([0, None])
+        plt.xlabel('Wavenumber (cm^-1)')
+        plt.ylabel('PSD (a.u)')
+        plt.title('Reconstructed spectrum')
+        plt.savefig('reconstructed_spectrum.png', dpi=300, bbox_inches='tight')
+
+mean_psd = np.mean(spectra, axis=1)
+
+# Work with phase spectra
+phase = np.mean(angle_spectra, axis=1)
+
 
 # Generate a timestamp
 timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -135,18 +158,28 @@ timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 filename = f"spectra_pc_{timestamp}.npy"
 
 
-ind_min = np.where(wn_axis < 2000)[0][-1]
-ind_max = np.where(wn_axis > 4500)[0][1]
+ind_min = np.where(freqs < 2000)[0][-1]
+ind_max = np.where(freqs > 4500)[0][1]
 
 # Save the array with the timestamp in the filename
 np.save(filename, spectra[:, ind_min:ind_max])
-np.save('wn_axis.npy', wn_axis[ind_min:ind_max])
+np.save('wn_axis.npy', freqs[ind_min:ind_max])
+
+np.save(filename, spectra)
+np.save('wn_axis.npy', freqs)
 
 plt.figure()
-background = np.mean(spectra, axis=1)
-plt.plot(wn_axis[ind_min:ind_max], background[ind_min:ind_max])
+plt.plot(freqs, mean_psd)
 plt.xlabel('Wavenumber (cm^-1)')
 plt.ylabel('PSD (a.u)')
 plt.title('Reconstructed spectrum')
 plt.xlim([2126, 3400])
 plt.ylim([0, None])
+plt.show()
+
+plt.figure()
+plt.plot(freqs, phase)
+plt.xlabel('Wavenumber (cm^-1)')
+plt.ylabel('Phase spectrum (a.u)')
+plt.title('Reconstructed spectrum')
+plt.show()
